ff1df4841e5c6d0b776055cd61df60da33da06e9
Divyam Solanki <solankidivyam00@gmail.com>
2020-10-12 13:28:09 +0530

    Third question

---
+++ question3.py
@@ -0,0 +1,11 @@
+#Licencing only
+
+class Solution(object):
+    def licenseKeyFormatting(self, S, K):
+        """
+        :type S: str
+        :type K: int
+        :rtype: str
+        """
+        S = S.replace("-", "").upper()[::-1]
+        return '-'.join(S[i:i+K] for i in range(0, len(S), K))[::-1]
